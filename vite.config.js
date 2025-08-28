d73ea039c7c4446c33969fd2cc6d69880f510b66
inus daimary <inusdaimary4@gmail.com>
2025-08-28 11:28:23 +0530

    as

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // server: {
+//   //   host: "0.0.0.0", 
+//   //   port: 5173,           
+//   // }
+
+  
+// })
+
+
+
 export default defineConfig({
+  base: "/nsfwebsite/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", 
-    port: 5173,           
-  }
-})
+});
